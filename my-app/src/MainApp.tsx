4f58de655c391cab55e585f3d0fd1a6d0f81a6a5
Giorgio <tran.giorgio@gmail.com>
2023-05-27 20:30:08 -1000

    Add fake contacts

--- my-app/src/MainApp.tsx
+++ my-app/src/MainApp.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+
+interface People {
+  name: string,
+  phoneNumber: string
+}
+
 const MainApp = () => {
+  const peopleList = [
+    {
+      name: "Jack",
+      phoneNumber: "8082222222"
+    },
+    {
+      name: "Jeffrey",
+      phoneNumber: "8089999999"
+    }
+  ]
+
+  const [contacts, setContacts] = useState<People[]>([])
+
+  useEffect(() => {
+    setContacts(peopleList);
+  }, []);
+
   return (
     <div className="parent-container">
       <div className="container">
-
+        
       </div>
     </div>
   )
