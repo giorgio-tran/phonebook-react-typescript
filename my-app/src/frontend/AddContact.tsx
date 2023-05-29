03a5a4cec1550215d02aa656af77df46443f11a0
Giorgio <tran.giorgio@gmail.com>
2023-05-28 23:47:46 -1000

    Change alignment of form

--- my-app/src/frontend/AddContact.tsx
+++ my-app/src/frontend/AddContact.tsx
@@ -10,28 +10,37 @@ const AddContact = ({ contacts, setContacts }: AddContactProps) => {
   const [name, setName] = useState<string>("")
   const [number, setNumber] = useState<string>("")
 
-  return (
-    <form onSubmit={(e) => {
-      e.preventDefault()
-      console.log({
-        name: name,
-        number: number
-      }, "test")}}
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log({
+      name: name,
+      number: number
+    })
+  }
 
-    >
-      <input 
-        value={name}
-        type="text"
-        onChange={(e) => {setName(e.target.value)}}
-        placeholder="enter name..."
-      />
-      <input
-        value={number}
-        type="text"
-        onChange={(e) => {setNumber(e.target.value)}}
-        placeholder="enter number..."
-      />
-      <button type="submit">
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <div className="input">
+        <label htmlFor="name">Name</label>
+        <input 
+          value={name}
+          id="name"
+          type="text"
+          onChange={(e) => {setName(e.target.value)}}
+          placeholder="enter name..."
+        />
+      </div>
+      <div className="input">
+        <label htmlFor="number">Number</label>
+        <input
+          value={number}
+          id="number"
+          type="text"
+          onChange={(e) => {setNumber(e.target.value)}}
+          placeholder="enter number..."
+        />
+      </div>
+      <button type="submit" className="add">
         Enter
       </button>
     </form>
